6eab0b9e39eb8b58003b459bf228dbe944bf7e89
JHern603 <jhern603@fiu.edu>
2022-06-16 10:07:03 -0400

    Added removerole core

--- cogs/roles.py
+++ cogs/roles.py
@@ -1,12 +1,9 @@
 
 import discord
-import random
 from discord.ext import commands
-
+import logging
 
 class roles(commands.Cog):
-
-    listylist=["cortadio","espresso","americano","italiano"]
 
     def __init__(self, client):
         self.client = client
@@ -15,9 +12,20 @@ class roles(commands.Cog):
     async def giverole(self, ctx):
         await ctx.send("giverole ran")
 
+    # Need to implement check to see if user has the role(s)
     @commands.command()
-    async def removerole(self, ctx):
-        await ctx.send("removerole ran")
+    async def removerole(self, ctx, user: discord.User, *roles):
+        user_roles = user.roles
+        await ctx.send(f"Removed the following roles from {str(user)}:")
+        for role in user_roles:
+            if str(role) != "@everyone":
+                if user_roles:
+                    ctx.send(f"{str(user)} does not have the {role} role!")
+                else:
+                    await user.remove_roles(role)
+                    logging.info(f"Removed{role} from {str(user)}")
+                    await ctx.send(f"\t{role}")
+                    
     @commands.command()
     async def getroles(self, ctx):
         for role in ctx.guild.roles:
